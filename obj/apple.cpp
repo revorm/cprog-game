88ec27bf15ad383688de44e8551de59860d3e05f
Mikael Gerdin <revorm@sovereign.(none)>
2010-01-10 17:30:40 +0100

    Fix whitespace

--- obj/apple.cpp
+++ obj/apple.cpp
@@ -4,7 +4,7 @@
 
 void cprog_game::Apple::interact(Character *c) {
   Examiner* e = 0;
-  if( (e  = dynamic_cast<Examiner*>(c)) ) {
+  if( (e = dynamic_cast<Examiner*>(c)) ) {
     e->set_good_mood();
   }
 }
